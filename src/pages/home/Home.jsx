eb651f5608d4ec467fdc4e11226d58ccaed082dd
harunmohamed <harunmohamed901@gmail.com>
2022-06-06 09:26:55 +0300

    header done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.css"
+import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/navbar/header/Header"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  )
+}
+
+export default Home
