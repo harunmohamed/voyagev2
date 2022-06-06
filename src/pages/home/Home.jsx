7a9cd8cb59b3c550b299adaf6b20800531ca791b
harunmohamed <harunmohamed901@gmail.com>
2022-06-06 14:07:14 +0300

    features hotels componnet

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import "./home.css"
 import Navbar from "../../components/navbar/Navbar"
 import Header from "../../components/header/Header"
+import Featured from "../../components/featured/Featured"
+
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured/>
+      </div>
     </>
   )
 }
