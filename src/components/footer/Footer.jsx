bec20bff35c3a2312acab8069c523ef2f1f83c1c
harunmohamed <harunmohamed901@gmail.com>
2022-06-06 14:54:01 +0300

    components done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Homes </li>
+          <li className="fListItem">Apartments </li>
+          <li className="fListItem">Resorts </li>
+          <li className="fListItem">Villas</li>
+          <li className="fListItem">Hostels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Unique places to stay </li>
+          <li className="fListItem">Unpacked: Travel articles </li>
+          <li className="fListItem">Travel communities </li>
+          <li className="fListItem">Seasonal and holiday deals </li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Car rental </li>
+          <li className="fListItem">Flight Finder</li>
+          <li className="fListItem">Restaurant reservations </li>
+          <li className="fListItem">Travel Agents </li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Customer Service</li>
+          <li className="fListItem">Sustainability</li>
+          <li className="fListItem">Safety Resource Center</li>
+          <li className="fListItem">Terms & conditions</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
